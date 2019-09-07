0d1329887d6bf55ab9cf01039537c24c97f26f47
huyemiem <54630019+huyemiem@users.noreply.github.com>
2019-09-07 19:07:46 +0700

    Add files via upload

---
+++ turtle15.py
@@ -0,0 +1,15 @@
+from turtle import *
+begin_fill()
+
+color('yellow')
+
+
+
+forward(100)
+left (120)
+forward(100)
+left(120)
+forward(100)
+end_fill()
+
+mainloop()
---
+++ turtle16.py
@@ -0,0 +1,10 @@
+from turtle import *
+
+begin_fill() 
+
+color('yellow')
+circle(100)
+
+end_fill()
+
+mainloop()
